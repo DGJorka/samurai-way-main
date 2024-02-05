e04243020fbb4811b0570a9e0f95c3614433bb16
George <GPPoskannyy@gmail.com>
2024-02-05 21:00:49 +0300

    12 lessons completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = ()=>{
+    return(
+    <header className='header'>
+        <img src="https://cdn.dribbble.com/users/5707693/screenshots/14382921/media/a132c9ed7ac236261ac781db73fae02b.jpg" alt=""/>
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = ()=>{
+    return(
+        <div className='content'>
+            <div>
+                <img src="https://jenmulligandesign.com/wp-content/uploads/2017/04/pexels-beach-tropical-scene-free-stock-photo.jpg" alt=""></img>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
